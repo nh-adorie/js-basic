4da112a20f468bcd38e07805f75794508b143350
Adorie <thuongnguyen271000@gmail.com>
2022-09-24 10:32:20 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log('Hải Thương')
+confirm('Hôm nay là thứ Bảy')
+prompt('Hôm nay là thứ mấy?')
+setTimeout(function() {
+    alert('Chúc bạn ăn ngon!')
+}, 1000)
+setInterval(function() {
+    alert('Ban da bi hack!')
+})
